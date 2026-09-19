7d3bb9636c3e418ec2b622759e462aee066f0e66
gdiaz <zalodiazg@gmail.com>
2026-09-18 23:30:24 -0300

    Agrega modo de construcción de casas a la Interfaz
    - Se agrega el botón para activar la construcción de casas
    - Se incorpora el estado de construcción en Interfaz
    - Se conecta el botón de construir casa con la Interfaz
    - Se mantienen las funcionalidades de selección de objetos
    - Se desarrollan estas funcionalidades mediante TDD

--- Colonia/src/interfaz/Interfaz.js
+++ Colonia/src/interfaz/Interfaz.js
@@ -1,12 +1,32 @@
 export class Interfaz {
-    constructor(canvas, mundo) {
+    constructor(canvas, mundo, botonConstruirCasa) {
         this.canvas = canvas
         this.mundo = mundo
+        this.botonConstruirCasa=botonConstruirCasa
         this.mouse = { x: 0, y: 0 }
         this.objetoSeleccionado = null
+        this.construyendoCasa = false
+
+        if (
+            botonConstruirCasa &&
+            typeof botonConstruirCasa.addEventListener === "function"
+        ) {
+            botonConstruirCasa.addEventListener("click", evento => {
+                this.construyendoCasa = true
+            })
+        }
 
         canvas.addEventListener("click", evento => {
-            this.objetoSeleccionado = this.objetoBajoMouse( )
+            this.objetoSeleccionado = this.objetoBajoMouse()
+            if (
+                this.mundo.colonias.length > 0 &&
+                this.objetoSeleccionado === this.mundo.colonias[0].ayuntamiento
+            ) {
+                this.botonConstruirCasa.hidden = false
+            }
+            else {
+                this.botonConstruirCasa.hidden = true
+            }
         })
 
         canvas.addEventListener("mousemove", evento => {
@@ -16,24 +36,33 @@ export class Interfaz {
             }
         })
     }
+    colisionPosicion(objeto) {
+        if (
+        objeto.posicion.x - objeto.ancho / 2 <= this.mouse.x &&
+        this.mouse.x <= objeto.posicion.x + objeto.ancho / 2 &&
+        objeto.posicion.y - objeto.alto <= this.mouse.y &&
+        this.mouse.y <= objeto.posicion.y
+        )
+        return true
+    }
     objetoBajoMouse() {
         for (const colono of this.mundo.colonos) {
-            if (
-                colono.posicion.x - colono.ancho / 2 <= this.mouse.x &&
-                this.mouse.x <= colono.posicion.x + colono.ancho / 2 &&
-                colono.posicion.y - colono.alto <= this.mouse.y &&
-                this.mouse.y <= colono.posicion.y
-            ) {
+            if (this.colisionPosicion(colono)) {
                 return colono
             }
         }
+        for (const colonia of this.mundo.colonias) {
+            if (this.colisionPosicion(colonia.ayuntamiento)) {
+                return colonia.ayuntamiento
+            }
+            for (const casa of colonia.casas.values()) {
+                if (this.colisionPosicion(casa)) {
+                    return casa
+                }
+            }
+        }
         for (const objeto of this.mundo.objetos) {
-            if (
-                objeto.posicion.x - objeto.ancho / 2 <= this.mouse.x &&
-                this.mouse.x <= objeto.posicion.x + objeto.ancho / 2 &&
-                objeto.posicion.y - objeto.alto <= this.mouse.y &&
-                this.mouse.y <= objeto.posicion.y
-            ) {
+            if (this.colisionPosicion(objeto)) {
                 return objeto
             }
         }
--- Colonia/tests/interfaz/Interfaz.test.js
+++ Colonia/tests/interfaz/Interfaz.test.js
@@ -35,7 +35,8 @@ describe("Interfaz", () => {
 
         const mundo = {
             objetos: [],
-            colonos: []
+            colonos: [],
+            colonias: []
         }
 
         const interfaz = new Interfaz(canvas, mundo)
@@ -67,7 +68,8 @@ describe("Interfaz", () => {
 
         const mundo = {
             objetos: [],
-            colonos: []
+            colonos: [],
+            colonias: []
         }
 
         const interfaz = new Interfaz(canvas, mundo)
@@ -99,7 +101,8 @@ describe("Interfaz", () => {
 
         const mundo = {
             colonos: [],
-            objetos: []
+            objetos: [],
+            colonias: []
         }
 
         const interfaz = new Interfaz(canvas, mundo)
@@ -139,12 +142,32 @@ describe("Interfaz", () => {
             }
         }
 
-        const mundo = {
-            objetos: [],
-            colonos: []
+        const botonConstruirCasa = {
+            hidden: true
+        }
+        const casa = {
+            posicion: { x: 400, y: 100 },
+            ancho: 100,
+            alto: 100
+        }
+        const colonia = {
+            casas: new Map([
+                ["Casa 1", casa]
+            ]),
+            ayuntamiento: {
+                posicion: {x:900, y:500},
+                ancho:200,
+                alto:200
+            }
         }
 
-        const interfaz = new Interfaz(canvas, mundo)
+        const mundo = {
+            objetos: [],
+            colonos: [],
+            colonias: [colonia]
+        }
+
+        const interfaz = new Interfaz(canvas, mundo, botonConstruirCasa)
 
         const objeto = {
             posicion: { x: 100, y: 100 },
@@ -162,5 +185,242 @@ describe("Interfaz", () => {
         canvas.funcionClick({})
 
         expect(interfaz.objetoSeleccionado).toBe(objeto)
+    })
+
+    it("Encuentra una casa bajo el mouse", () => {
+        const canvas = {
+            addEventListener: (evento, funcion) => {
+                if (evento === "mousemove") {
+                    canvas.funcionMouse = funcion
+                }
+            }
+        }
+
+        const casa = {
+            posicion: { x: 100, y: 100 },
+            ancho: 100,
+            alto: 100
+        }
+
+        const colonia = {
+            casas: new Map([
+                ["Casa 1", casa]
+            ]),
+            ayuntamiento: {
+                posicion: {x:300, y:500},
+                ancho:200,
+                alto:200
+            }
+        }
+
+        const mundo = {
+            objetos: [],
+            colonos: [],
+            colonias: [colonia]
+        }
+
+        const interfaz = new Interfaz(canvas, mundo)
+
+        canvas.funcionMouse({
+            clientX: 100,
+            clientY: 50
+        })
+
+        expect(interfaz.objetoBajoMouse()).toBe(casa)
+    })
+    it("Encuentra el ayuntamiento bajo el mouse", () => {
+        const canvas = {
+            addEventListener: (evento, funcion) => {
+                if (evento === "mousemove") {
+                    canvas.funcionMouse = funcion
+                }
+            }
+        }
+
+        const ayuntamiento = {
+            posicion: { x: 100, y: 100 },
+            ancho: 200,
+            alto: 200
+        }
+
+        const colonia = {
+            casas: new Map(),
+            ayuntamiento: ayuntamiento
+        }
+
+        const mundo = {
+            objetos: [],
+            colonos: [],
+            colonias: [colonia]
+        }
+
+        const interfaz = new Interfaz(canvas, mundo)
+
+        canvas.funcionMouse({
+            clientX: 100,
+            clientY: 50
+        })
+
+        expect(interfaz.objetoBajoMouse()).toBe(ayuntamiento)
+    })
+    it("Selecciona el ayuntamiento al hacer click", () => {
+        const canvas = {
+            addEventListener: (evento, funcion) => {
+                if (evento === "mousemove") {
+                    canvas.funcionMouse = funcion
+                }
+
+                if (evento === "click") {
+                    canvas.funcionClick = funcion
+                }
+            }
+        }
+
+        const ayuntamiento = {
+            posicion: { x: 100, y: 100 },
+            ancho: 200,
+            alto: 200
+        }
+
+        const colonia = {
+            casas: new Map(),
+            ayuntamiento: ayuntamiento
+        }
+
+        const mundo = {
+            objetos: [],
+            colonos: [],
+            colonias: [colonia]
+        }
+        const botonConstruirCasa = {
+            hidden: true
+        }
+
+        const interfaz = new Interfaz(canvas, mundo, botonConstruirCasa)
+
+        canvas.funcionMouse({
+            clientX: 100,
+            clientY: 50
+        })
+
+        canvas.funcionClick({})
+
+        expect(interfaz.objetoSeleccionado).toBe(ayuntamiento)
+    })
+
+    it("Muestra el botón de construir casa al seleccionar el ayuntamiento", () => {
+        const canvas = {
+            addEventListener: (evento, funcion) => {
+                if (evento === "mousemove") {
+                    canvas.funcionMouse = funcion
+                }
+
+                if (evento === "click") {
+                    canvas.funcionClick = funcion
+                }
+            }
+        }
+
+        const botonConstruirCasa = {
+            hidden: true
+        }
+
+        const ayuntamiento = {
+            posicion: { x: 100, y: 100 },
+            ancho: 200,
+            alto: 200
+        }
+
+        const colonia = {
+            casas: new Map(),
+            ayuntamiento: ayuntamiento
+        }
+
+        const mundo = {
+            objetos: [],
+            colonos: [],
+            colonias: [colonia]
+        }
+
+        const interfaz = new Interfaz(canvas, mundo, botonConstruirCasa)
+
+        canvas.funcionMouse({
+            clientX: 100,
+            clientY: 50
+        })
+
+        canvas.funcionClick({})
+
+        expect(botonConstruirCasa.hidden).toBe(false)
+    })
+
+    it("Oculta el botón de construir casa al seleccionar otro objeto", () => {
+        const canvas = {
+            addEventListener: (evento, funcion) => {
+                if (evento === "mousemove") {
+                    canvas.funcionMouse = funcion
+                }
+
+                if (evento === "click") {
+                    canvas.funcionClick = funcion
+                }
+            }
+        }
+
+        const botonConstruirCasa = {
+            hidden: false
+        }
+
+        const objeto = {
+            posicion: { x: 100, y: 100 },
+            ancho: 50,
+            alto: 50
+        }
+
+        const mundo = {
+            objetos: [objeto],
+            colonos: [],
+            colonias: []
+        }
+
+        const interfaz = new Interfaz(canvas, mundo, botonConstruirCasa)
+
+        canvas.funcionMouse({
+            clientX: 100,
+            clientY: 75
+        })
+
+        canvas.funcionClick({})
+
+        expect(botonConstruirCasa.hidden).toBe(true)
+    })
+
+    it("Activa el modo de construir casa al hacer click en el botón", () => {
+        const canvas = {
+            addEventListener: (evento, funcion) => {
+                if (evento === "click") {
+                    canvas.funcionClick = funcion
+                }
+            }
+        }
+
+        const botonConstruirCasa = {
+            hidden: false,
+            addEventListener: (evento, funcion) => {
+                botonConstruirCasa.funcionClick = funcion
+            }
+        }
+
+        const mundo = {
+            objetos: [],
+            colonos: [],
+            colonias: []
+        }
+
+        const interfaz = new Interfaz(canvas, mundo, botonConstruirCasa)
+
+        botonConstruirCasa.funcionClick({})
+
+        expect(interfaz.construyendoCasa).toBe(true)
     })
 })
